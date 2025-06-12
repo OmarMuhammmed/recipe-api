48be523231ca84c7448de278cf0c8e526370b77e
OmarMuhammmed <amorey2006@gmail.com>
2025-06-12 19:30:03 +0300

    configured docker compose and checks to wait for DB

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,33 +1,34 @@
 """
-Test custom Django management commands
+Test custom Django management commands.
 """
 from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2Error
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Test commands"""
+    """Test commands."""
 
-    @patch('django.db.connections')
-    def test_wait_for_db_ready(self, patched_connections):
-        """Test waiting for database if database ready"""
-        patched_connections.__getitem__.return_value.cursor.return_value.__enter__.return_value = True
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
 
         call_command('wait_for_db')
 
-        patched_connections.__getitem__.assert_called_once_with('default')
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    @patch('django.db.connections')
-    def test_wait_for_db_delay(self, patched_connections, patched_sleep):
-        """Test waiting for database when getting OperationalError"""
-        patched_connections.__getitem__.return_value.cursor.side_effect = [Psycopg2Error] * 2 + \
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_connections.__getitem__.call_count, 6)
-        patched_connections.__getitem__.assert_called_with('default')
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
