0642410d49a63f81b3699b9d0462d910ae5f700e
OmarMuhammmed <amorey2006@gmail.com>
2025-06-22 23:33:45 +0300

    feat:Build image API

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@
 Database Models
 """
 import uuid
-import os 
+import os
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -11,12 +11,14 @@ from django.contrib.auth.models import (
 )
 from django.conf import settings
 
+
 def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image"""
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
 
     return os.path.join('uploads/recipe/', filename)
+
 
 class UserManger(BaseUserManager):
     """Manger for Users"""
